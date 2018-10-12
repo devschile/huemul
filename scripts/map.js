23f8e4713f7e1bec1ad4daf6753c9fb015bf789c
Leonardo Gatica <lgatica@protonmail.com>
2018-10-12 17:26:10 -0300

    feat(map): add verification of result

--- scripts/map.js
+++ scripts/map.js
@@ -21,6 +21,21 @@ module.exports = function(robot) {
   }
 
   robot.respond(/map (.+)/i, function(msg) {
+    const send = attachment => {
+      if (robot.adapter.constructor.name === 'SlackBot') {
+        const options = {
+          as_user: false,
+          link_names: 1,
+          icon_url: 'https://www.google.com/images/branding/product/2x/maps_96in128dp.png',
+          username: 'Google Maps',
+          unfurl_links: false,
+          attachments: [attachment]
+        }
+        robot.adapter.client.web.chat.postMessage(msg.message.room, null, options)
+      } else {
+        msg.send(attachment.fallback)
+      }
+    }
     const location = msg.match[1]
     const mapUrlParams = querystring.stringify({
       center: location,
@@ -34,26 +49,21 @@ module.exports = function(robot) {
     const urlParams = querystring.stringify({ q: location, t: 'm', z: 12 })
     const url = `https://www.google.com/maps?${urlParams}`
     const text = `:world_map: ${url}\n:frame_with_picture: ${mapUrl}`
-    if (robot.adapter.constructor.name === 'SlackBot') {
-      const options = {
-        as_user: false,
-        link_names: 1,
-        icon_url: 'https://www.google.com/images/branding/product/2x/maps_96in128dp.png',
-        username: 'Google Maps',
-        unfurl_links: false,
-        attachments: [
-          {
-            fallback: text,
-            color: 'good',
-            title: location,
-            title_link: url,
-            image_url: mapUrl
-          }
-        ]
+    robot.http(mapUrl).get()((err, resp) => {
+      if (resp.headers['x-staticmap-api-warning'] === 'Error geocoding: center' || resp.statusCode !== 200 || err) {
+        return send({
+          fallback: `La dirección "${location}" no retorna resultados`,
+          color: 'danger',
+          text: `La dirección *${location}* no retorna resultados`
+        })
       }
-      robot.adapter.client.web.chat.postMessage(msg.message.room, null, options)
-    } else {
-      msg.send(text)
-    }
+      send({
+        fallback: text,
+        color: 'good',
+        title: location,
+        title_link: url,
+        image_url: mapUrl
+      })
+    })
   })
 }
