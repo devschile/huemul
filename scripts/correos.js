a0b471cc361ff2b064f3580ed1860bc0bba10fb1
Matías Giménez <matias.gimenez@ubiome.com>
2018-11-19 16:05:32 -0300

    feat(correos): detect 26 char. code and convert it to valid code

--- scripts/correos.js
+++ scripts/correos.js
@@ -22,7 +22,10 @@ module.exports = robot => {
       }
     }
     const sendError = (options, message) => {
-      options.attachments[0].fallback = message || 'Búsqueda sin resultados'
+      options.attachments[0].fallback =
+        message +
+          '. Solo aparecerán los estados de los envíos que ya ingresaron a Chile y fueron recepcionados por la empresa.' ||
+        'Búsqueda sin resultados'
       options.attachments[0].text = options.attachments[0].fallback
       options.attachments[0].color = 'danger'
       send(options)
@@ -37,7 +40,11 @@ module.exports = robot => {
     }
 
     try {
-      const results = await correos([res.match[1]])
+      let trackingCode = res.match[1]
+      if (trackingCode.length === 26) {
+        trackingCode = trackingCode.replace(/\w+(\d{12})\d{3}$/, '$1')
+      }
+      const results = await correos([trackingCode])
       if (results.length === 0) return sendError(options)
       if (typeof results[0] === 'string') return sendError(options, results[0])
       if (results[0].registros.length === 0) sendError(options)
@@ -46,9 +53,12 @@ module.exports = robot => {
         const dateB = new Date(b.fecha.replace(/(\d+)\/(\d+)\/(\d+) (\d+:\d+)/, '$3-$2-$1 $4'))
         return dateA < dateB
       })[0]
-      const text = [`- Envío: ${res.match[1]}`, `- Estado: ${estado}`, `- Fecha: ${fecha}`, `- Lugar: ${lugar}`].join(
-        '\n'
-      )
+      const text = [
+        `- Envío: ${trackingCode} (${res.match[1]})`,
+        `- Estado: ${estado}`,
+        `- Fecha: ${fecha}`,
+        `- Lugar: ${lugar}`
+      ].join('\n')
       options.attachments[0].fallback = text
       options.attachments[0].color = 'good'
       options.attachments[0].fields = [
