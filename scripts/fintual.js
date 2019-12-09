5dc66aead780ae3f1ce5ee4af693dd1a225f4595
Carlos Vallejos <cvallejosmenares@gmail.com>
2019-12-08 23:44:05 -0300

    WIP: Get data between last 30 days

--- scripts/fintual.js
+++ scripts/fintual.js
@@ -1,66 +1,55 @@
 // Description:
 //   Obtiene valores de los distintos fondos de la AGF Fintual
 //
-// Dependencies:
-//   numberToCLPFormater
-//
-// Configuration:
-//   None
 //
 // Commands:
 //   hubot fintual help - Imprime la ayuda
-//   hubot fintual risky-a|risky-apv|clooney-a|clooney-apv|Very Conservative Streep - Obtiene el valor del fondo seleccionado
+//   hubot fintual risky-a|risky-apv|clooney-a|clooney-apv|streep - Obtiene el valor del fondo seleccionado
 //
 // Author:
 //   @cvallejos
 
-const CLP = require('numbertoclpformater').numberToCLPFormater
 const API_URL = process.env.API_URL || 'https://fintual.cl/api/real_assets/'
-const mensajes = [
+/* const mensajes = [
   'Aunque te esfuerces, seguirás siendo pobre. :poop:',
   'Los políticos ganan más que tú y más encima nos roban. Y no pueden irse presos. ¡Ánimo! :monkey:',
   'La economía seguirá mal para ti, pero no para tu AFP. :moneybag:',
   'Algún día saldrás de la clase media. Partiste a jugarte un LOTO. :alien:',
   'Todos los días suben los precios, y no tu sueldo. :money_with_wings:'
-]
+] */
 
-const NORRIS_A = 'norris-a';
-const NORRIS_APV = 'norris-apv';
-const PITT_A = 'pitt-a';
-const PITT_APV = 'pitt-apv';
-const CLOONEY_A = 'clooney-a';
-const CLOONEY_APV = 'clooney-apv';
-const STREEP_A = 'streep-a';
+var NORRIS_A = 'norris-a'
+var NORRIS_APV = 'norris-apv'
+var PITT_A = 'pitt-a'
+var PITT_APV = 'pitt-apv'
+var CLOONEY_A = 'clooney-a'
+var CLOONEY_APV = 'clooney-apv'
+var STREEP_A = 'streep-a'
 
-const series = {
-    NORRIS_A : 186,
-    NORRIS_APV: 245,
-    PITT_A: 187,
-    PITT_APV: 246,
-    CLOONEY_A: 188,
-    CLOONEY_APV: 247,
-    STREEP_A : 15077,
-};
+const series = []
+series[NORRIS_A] = 186
+series[NORRIS_APV] = 245
+series[PITT_A] = 187
+series[PITT_APV] = 246
+series[CLOONEY_A] = 188
+series[CLOONEY_APV] = 247
+series[STREEP_A] = 15077
 
-  const comands = [  NORRIS_A,NORRIS_APV,PITT_A,PITT_APV,CLOONEY_A,CLOONEY_APV,STREEP_A]
-
-const numberWithCommas = number => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-
-const numberSplitDecimal = number => {
-  const d = Math.pow(10, 2)
-  return (parseInt(number * d, 10) / d).toFixed(number)
-}
+const comands = [NORRIS_A, NORRIS_APV, PITT_A, PITT_APV, CLOONEY_A, CLOONEY_APV, STREEP_A]
 
 module.exports = robot => {
-  robot.respond(/fintual (\w+)/i, res => {
+  robot.respond(/fintual (\w+)[-](\w+)/i, res => {
     let uri
-    const indicador = res.match[1].toLowerCase()
-
-    if (indicadores.includes(indicador)) {
-      uri = API_URL + series[indicador] + '/'
+    const indicador = res.match[0].replace('huemul fintual ', '').toLowerCase()
+    res.send(indicador)
+    if (comands.includes(indicador)) {
+      const today = new Date()
+      const endDate = new Date()
+      endDate.setDate(today.getDate() - 30)
+      uri = API_URL + series[indicador] + '/days?from_date=' + endDate.toISOString().slice(0, 10) + '&to_date=' + today.toISOString().slice(0, 10)
     } else {
       res.send(
-        'Mis comandos son:\n\n * `fintual '+NORRIS_A+'|'+ NORRIS_APV +'`\n * `fintual '+PITT_A+'|'+PITT_APV+'`\n * `fintual '+ CLOONEY_A +'|'+CLOONEY_APV+'`\n * `fintual '+STREEP_A+'`\n'
+        'Mis comandos son:\n\n * `fintual ' + NORRIS_A + '|' + NORRIS_APV + '`\n * `fintual ' + PITT_A + '|' + PITT_APV + '`\n * `fintual ' + CLOONEY_A + '|' + CLOONEY_APV + '`\n * `fintual ' + STREEP_A + '`\n'
       )
       return false
     }
@@ -73,46 +62,21 @@ module.exports = robot => {
       }
 
       response.setEncoding('utf-8')
-      let data = JSON.parse(body)
-      const lastDate =new Date(data[0].attributes.date.split('T')[0]) 
-      const lastDate = lastDate
-
+      const data = JSON.parse(body)
       if (!data && comands.includes(indicador)) {
         return res.send('Sin resultados')
       }
-      switch(indicador){
-        case NORRIS_A:
-            data = CLP(data.attribute.last_day.valor)
-            break;
-        case NORRIS_APV:
-            data = CLP(data.uf.valor)
-            break;          
-        case PITT_A:
-            data = CLP(data.uf.valor)
-            break;          
-        case PITT_APV:
-            data = CLP(data.uf.valor)
-            break;
-        case CLOONEY_A:
-            data = CLP(data.uf.valor)
-            break;
-        case CLOONEY_APV:
-            data = CLP(data.uf.valor)
-            break;
-        case STREEP_A:
-            data = CLP(data.uf.valor)
-            break;
-        default:
-            data = '`fintual help` para ayuda.'
-
-      }       
-      if (data !== null && typeof data !== 'object') {
-        data = data.toString().split(',', 1)
-        const mensaje = res.random(mensajes)
-        res.send(`${indicador.toUpperCase()}: ${data} (${date}). ${mensaje}`)
+      const firstElement = data.data[0]
+      const lastElement = data.data[data.data.length - 1]
+      const retorno = parseFloat(((lastElement.attributes.price / firstElement.attributes.price) - 1) * 100).toFixed(2)
+      if (!isNaN(retorno)) {
+        const rentabilidad = retorno + '%'
+        // const mensaje = res.random(mensajes)
+        res.send(`${indicador.toUpperCase()}: La rentabilidad del ultimo mes fue de ${rentabilidad}`)
       } else {
         res.send('Error, intenta nuevamente *:c3:*.')
       }
     })
-  })
+  }
+  )
 }
