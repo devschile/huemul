cddf3044cf8f48c720dac62d70c10d6d122a479a
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2020-10-30 21:39:48 -0300

    feat: adds a script for taxes & contributions

---
+++ scripts/impuestos.js
@@ -0,0 +1,33 @@
+const { calcular, obtenerTramoImpositivo } = require('tax-cl')
+const { numberToCLPFormater } = require('numbertoclpformater')
+
+module.exports = robot => {
+  robot.respond(/impuestos (.*)/i, msg => {
+    const income = parseInt(msg.match[1], 10)
+
+    const {
+      deuda,
+      retencion,
+      impuestos,
+      montoCotizacionesObligatorias: cotizaciones,
+      deudaModalidadParcial: parcial,
+      sueldoTributable
+    } = calcular(income)
+
+    const tramoImpositivo = obtenerTramoImpositivo(sueldoTributable)
+    const emoji = tramoImpositivo.factor > 0.04 ? ':patrones:' : ':c3:'
+
+    const messages = [
+      deuda > 0
+        ? `Tendrás que pagar ~ *${numberToCLPFormater(deuda)}* ${emoji}`
+        : `Recibirás una devolución de *${numberToCLPFormater(-deuda)}*`,
+      `A favor tienes tu retención de boletas (10.75%): *${numberToCLPFormater(retencion)}*`,
+      `En contra tienes los impuestos (*${numberToCLPFormater(impuestos)}*) y las cotizaciones (*${numberToCLPFormater(cotizaciones)}*)`,
+      parcial > 0
+        ? `Si optas por modalidad parcial, tendrás que pagar ~ *${numberToCLPFormater(parcial)}*`
+        : `Si optas por modalidad parcial, recibirás una devolución de ~ *${numberToCLPFormater(-parcial)}*`
+    ]
+
+    msg.send(messages.join('\n- '))
+  })
+}
