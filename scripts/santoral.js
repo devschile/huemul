d9b572b68def7aab41f6141ba644187b0a13735a
Jorge Epuñan <juanbrujo@gmail.com>
2021-01-25 13:01:40 -0300

    refactor: as suggested in code review

--- scripts/santoral.js
+++ scripts/santoral.js
@@ -14,20 +14,17 @@ const moment = require('moment')
 
 const url = 'https://gist.githubusercontent.com/juanbrujo/c27f9bea0f5a7ac56802122ec50d5a9b/raw/1e7de22b3d71db061f6ea1e2f3495ecd453c94ff/santorales-catolicos.json'
 
-const currentMonth = moment().format('MMMM')
-const currentDay = moment().format('D')
-const weekDay = moment().format('dddd')
-
 module.exports = robot => {
   robot.respond(/santoral/i, msg => {
+    const currentMonth = moment().format('MMMM')
+    const currentDay = moment().format('D')
+    const weekDay = moment().format('dddd')
+
     robot.http(url).get()((error, response, body) => {
-      if (response.statusCode !== 200 || !error) {
+      if (response.statusCode === 200 || !error) {
         const santoral = JSON.parse(body)
 
-        Object.keys(santoral).map(function(key) {
-          if (key === currentMonth)
-            msg.send(`Hoy *${weekDay} ${currentDay} de ${currentMonth}* el santoral es *${santoral[key][currentDay - 1]}* :angel:`)
-        })
+        msg.send(`Hoy *${weekDay} ${currentDay} de ${currentMonth}* el santoral es *${santoral[currentMonth][currentDay - 1]}* :angel:`)
       } else {
         throw new Error(error)
       }
